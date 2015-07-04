0f6175fcae2a33e14ca662dfa40632f7ba829d7d
Marlon Cárdenas <marlon.cb@gmail.com>
2015-07-04 21:16:16 +0200

    Layout para las pantallas

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var partial = require('express-partials');
 var routes = require('./routes/index');
 //var users = require('./routes/users');
 
@@ -24,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 //app.use('/users', users);
+
+app.use(partial());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
