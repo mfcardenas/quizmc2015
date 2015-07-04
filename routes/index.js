cea9de70e36932429731df9830b7fe5c57cd0e5b
Marlon Cárdenas <marlon.cb@gmail.com>
2015-07-04 20:28:28 +0200

    Primera Pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/quiz/question',quizController.question);
+router.get('/quiz/answer',quizController.answer);
 
 module.exports = router;
